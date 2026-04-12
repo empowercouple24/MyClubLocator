746be3d389bd9c5cb0e6c6cbbb017321b426181e
empowercouple24 <empowercouple24@gmail.com>
2026-04-12 03:37:40 -0400

    Add files via upload

---
+++ src/lib/leaflet-esm.js
@@ -0,0 +1,3 @@
+import * as L from '../../node_modules/leaflet/dist/leaflet-src.esm.js'
+export * from '../../node_modules/leaflet/dist/leaflet-src.esm.js'
+export default L
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Force leaflet to use its ESM build (all `var`, no TDZ issues)
+      // instead of the UMD/CJS build that Rollup mangles
+      'leaflet': path.resolve(__dirname, 'src/lib/leaflet-esm.js'),
+    },
+  },
 })
