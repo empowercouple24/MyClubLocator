d221e260b187312d67193cc7d507c9458a34e71b
empowercouple24 <empowercouple24@gmail.com>
2026-04-12 10:12:15 -0400

    Delete migration-007-welcome-disclaimer.sql

--- migration-007-welcome-disclaimer.sql
+++
@@ -1,6 +0,0 @@
--- Migration 007: Welcome modal disclaimer + video placeholder
--- Run this in your Supabase SQL Editor
-
-ALTER TABLE app_settings
-  ADD COLUMN IF NOT EXISTS welcome_disclaimer        TEXT DEFAULT '',
-  ADD COLUMN IF NOT EXISTS welcome_video_placeholder TEXT DEFAULT 'https://www.youtube.com/embed/dQw4w9WgXcQ';
